40b1550fb65e1cd7bb7428a9e51871f36c1a3bc7
mistrywoman <komal.mistry224@gmail.com>
2020-08-23 19:33:05 -0700

    changing to findall

--- q1.py
+++ q1.py
@@ -3,14 +3,15 @@ import re
 def reg(input_string, pattern, dot):
     "Given an input string , pattern and dot or star matching rule returns all the matches found"
     if dot:
-        res = re.match(pattern+'.', input_string)
+        res = re.findall(pattern+'.', input_string)
     else:
-        res = re.match('(' + pattern + ')*', input_string)
+        res = re.findall(pattern + '*', input_string)
     return res
 
 
-test1 = reg('boomboom', 'boom', dot = True)
+test1 = reg('boomboomboom', 'boom', dot = True)
 print(test1)
 
-test2 = reg('boomboom', 'boom', dot = False)
+test2 = reg('boomboomboom', 'boom', dot = False)
 print(test2)
+
