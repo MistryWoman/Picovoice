52061179c42553f6b157f1f75ee65eda9dda2489
mistrywoman <komal.mistry224@gmail.com>
2020-08-23 19:24:50 -0700

    pushing initial function

--- q1.py
+++ q1.py
@@ -1,2 +1,16 @@
 import re
-print('hi')
+
+def reg(input_string, pattern, dot):
+    "Given an input string , pattern and dot or star matching rule returns all the matches found"
+    if dot:
+        res = re.match(pattern+'.', input_string)
+    else:
+        res = re.match('(' + pattern + ')*', input_string)
+    return res
+
+
+test1 = reg('boomboom', 'boom', dot = True)
+print(test1)
+
+test2 = reg('boomboom', 'boom', dot = False)
+print(test2)
